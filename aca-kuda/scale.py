f8bef75e7eb7945397184e10a67eaf178f677e4d
scottd <scottd@example.com>
2026-05-25 16:59:32 +0100

    scale to 100

--- aca-kuda/scale.py
+++ aca-kuda/scale.py
@@ -4,6 +4,6 @@ conn_str = "<your-endpoint-connection-string>"
 
 with ServiceBusClient.from_connection_string(conn_str) as client:
     with client.get_queue_sender("orders") as sender:
-        for i in range(20):
+        for i in range(100):
             sender.send_messages(ServiceBusMessage(f"Order {i}"))
-print("Sent 20 messages")
+print("Sent 100 messages")
